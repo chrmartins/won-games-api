56b461375d3d595ed33599017026bbaeeeeeaa53
chrmartins <cmts2010@gmail.com>
2020-09-02 16:28:28 -0300

    Initial

---
+++ admin/src/config.js
@@ -0,0 +1,2 @@
+﻿export const SHOW_TUTORIALS = false;
+export const SETTINGS_BASE_URL = '/settings';
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6747391f-7c68-4407-99ae-9efc5e6d6661'
+};
